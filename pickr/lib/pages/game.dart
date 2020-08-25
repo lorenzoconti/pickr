3058f62ec9477726b3f666763632fdcb564b59b1
lorenzoconti <dev.lorenzoconti@gmail.com>
2020-08-25 15:35:43 +0200

    lobby

--- pickr/lib/pages/game.dart
+++ pickr/lib/pages/game.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:pickr/handlers/game.dart';
-import 'package:pickr/providers/game-provider.dart';
 
 class GamePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //
-    GameSession game = GameProvider.of(context).game;
+    //GameSession game = GameProvider.of(context).game;
 
     return Scaffold(
       appBar: AppBar(title: Text("Game")),
