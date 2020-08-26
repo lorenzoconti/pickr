966e500107f96a181c6c08793166482a8a95316d
lorenzoconti <dev.lorenzoconti@gmail.com>
2020-08-26 18:54:41 +0200

    fixed_test_widget_auth

--- pickr/test/widget/auth_page_test.dart
+++ pickr/test/widget/auth_page_test.dart
@@ -3,23 +3,30 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
 import 'package:pickr/handlers/auth.dart';
+import 'package:pickr/handlers/game.dart';
 import 'package:pickr/pages/auth.dart';
 import 'package:pickr/providers/auth-provider.dart';
+import 'package:pickr/providers/game-provider.dart';
 
 class MockAuth extends Mock implements BaseAuth {}
 
+class MockGame extends Mock implements GameSessionInterface {}
+
 void main() {
   //
-  Widget app(Widget child, BaseAuth auth) =>
-      AuthProvider(auth: auth, child: MaterialApp(home: child));
+  Widget app(Widget child, BaseAuth auth, GameSessionInterface game) =>
+      AuthProvider(
+          auth: auth,
+          child: GameProvider(game: game, child: MaterialApp(home: child)));
 
   testWidgets('Empty Email and Password: Do not sign in.',
       (WidgetTester tester) async {
     //
     MockAuth mockAuth = MockAuth();
+    MockGame mockGame = MockGame();
     AuthPage page = AuthPage();
 
-    await tester.pumpWidget(app(page, mockAuth));
+    await tester.pumpWidget(app(page, mockAuth, mockGame));
 
     await tester.tap(find.byKey(Key('login_button')));
 
@@ -29,9 +36,10 @@ void main() {
   testWidgets('Valid Email and Password: Sign in', (WidgetTester tester) async {
     //
     MockAuth mockAuth = MockAuth();
+    MockGame mockGame = MockGame();
     AuthPage page = AuthPage();
 
-    await tester.pumpWidget(app(page, mockAuth));
+    await tester.pumpWidget(app(page, mockAuth, mockGame));
 
     Finder emailField = find.byKey(Key('email'));
     await tester.enterText(emailField, 'email@test.com');
@@ -50,9 +58,10 @@ void main() {
       (WidgetTester tester) async {
     //
     MockAuth mockAuth = MockAuth();
+    MockGame mockGame = MockGame();
     AuthPage page = AuthPage();
 
-    await tester.pumpWidget(app(page, mockAuth));
+    await tester.pumpWidget(app(page, mockAuth, mockGame));
 
     Finder emailField = find.byKey(Key('email'));
     await tester.enterText(emailField, 'emailtestcom');
